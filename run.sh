72100e953c349d8b5bf078599c679c7ea9ecfbf9
Klemens Lechner <p2467946@hpc-login1.xcat.local>
2025-11-15 11:50:42 +0100

    Works on cluster

--- run.sh
+++ run.sh
@@ -9,13 +9,13 @@
 #SBATCH -p p2gpu
 #SBATCH --qos p2gpu
 #SBATCH --time 7-0:0:0
-#SBATCH -o /mulfs/home/p2467946/aaa_New/GNN_Gym/slurm_output_%j.out
-#SBATCH -e /mulfs/home/p2467946/aaa_New/GNN_Gym/slurm_error_%j.err
+#SBATCH -o slurm_output_%j.out
+#SBATCH -e slurm_error_%j.err
 #---
 ulimit -s unlimited
 
-# Change to working directory
-cd /mulfs/home/p2467946/aaa_New/GNN_Gym
+# Change to directory from which sbatch was called
+cd $SLURM_SUBMIT_DIR
 
 # Activate conda/mamba
 source /mulfs/home/p2467946/mambaforge/etc/profile.d/conda.sh
